e9e32511e1831b2f805d81247f0672d5348647b6
Georgi Asenov <gasenov@weband.bg>
2019-10-20 03:35:15 +0300

    setup/creating project architecture

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
+    <general-layout></general-layout>
   </div>
 </template>
 
 <script>
+import GeneralLayout from './layouts/GeneralLayout'
+
 export default {
   name: 'app',
   components: {
-  }
+    generalLayout: GeneralLayout,
+  }  
 }
 </script>
 
 <style lang="scss">
+@import "assets/styles/main.scss";
 </style>
---
+++ src/layouts/GeneralLayout.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <section class="options">
+            <button class="v-button save">save</button>
+            <div class="filter">
+                <!-- multiselect -->
+                <button class="v-button check">check all</button>
+                <button class="v-button remove">remove all</button>
+            </div>
+            
+        </section>
+        <section class="bookmakers">
+            <app-bookmaker></app-bookmaker>
+        </section>
+    </div>
+</template>
+
+<script>
+import Bookmaker from '@/components/Bookmaker'
+export default {
+    components: {
+        appBookmaker: Bookmaker
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        background-color: #F1F3F9;
+    }
+</style>
